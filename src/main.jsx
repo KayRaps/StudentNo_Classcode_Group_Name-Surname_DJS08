c3c5d5365d9ccd73496d1139dcf7d49bb18314be
Karabo Edward Mokgatle <karaboedward64@gmail.com>
2024-06-03 15:25:07 +0200

    added imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import Home from "./components/Home"
+import About from "./components/About"
 import App from './App.jsx'
 import './index.css'
 
