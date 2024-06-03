3f923cbd14a5fbe54932110b861a4ece0be6d03d
Karabo Edward Mokgatle <karaboedward64@gmail.com>
2024-06-03 15:30:35 +0200

    added a function APP to return the browseer router

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
-import Home from "./components/Home"
-import About from "./components/About"
-import App from './App.jsx'
-import './index.css'
+import Home from "./pages/Home"
+import About from "./pages/About"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+function App() {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/about" element={<About />}/>
+        </Routes>
+      </BrowserRouter>
+    )
+  }
+
+ReactDOM
+  .createRoot(document.getElementById('root'))
+  .render(<App />);
