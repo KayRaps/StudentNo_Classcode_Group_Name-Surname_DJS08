7d27d62427d876c6a9995cbbca37e739b079ecda
Karabo Edward Mokgatle <karaboedward64@gmail.com>
2024-06-03 15:33:25 +0200

    added link

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/about" element={<About />}/>
+          <Route path="/link" element={Link />}/>
         </Routes>
       </BrowserRouter>
     )
