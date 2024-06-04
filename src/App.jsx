5733f7230acb4d141e555e7d2e040ca92be3031b
Karabo Edward Mokgatle <karaboedward64@gmail.com>
2024-06-04 16:25:53 +0200

    added an import

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./components/Home"
 import About from "./components/About"
+import Vans from "./components/Vans"
+
+import "./server"
 
 function App() {
     return (
