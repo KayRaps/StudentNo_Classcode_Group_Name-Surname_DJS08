cfc182a65a8b601b7acd2a3693798c17e1643d25
Karabo Edward Mokgatle <karaboedward64@gmail.com>
2024-06-05 16:14:02 +0200

    Fetch van details and log to console in VanDetail component

--- src/components/VanDetail.jsx
+++ src/components/VanDetail.jsx
@@ -7,7 +7,10 @@ export default function vanDetail() {
 
 
     React.useEffect(() => {
+        fetch(`/api/vans/${params.id}`)
+            .then(res => res.json())
+            .then(data => console.log(data))
 
-    }, [])
+    }, [params.id])
     return <h1>Van detail page goes here</h1>
 }
