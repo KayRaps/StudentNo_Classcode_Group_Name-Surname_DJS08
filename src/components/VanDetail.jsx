8294dd80ad046b6e4414133ebf0687ec31fb9ecb
Karabo Edward Mokgatle <karaboedward64@gmail.com>
2024-06-05 16:00:22 +0200

    added a function for syncronising purposes.

--- src/components/VanDetail.jsx
+++ src/components/VanDetail.jsx
@@ -4,5 +4,10 @@ import { useParams } from "react-router-dom"
 export default function vanDetail() {
     const params = useParams()
     console.log(params)
+
+
+    React.useEffect(() => {
+
+    }, [])
     return <h1>Van detail page goes here</h1>
 }
