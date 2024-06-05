e9169c53b8c1e2e7c0e327102505f5b6d9021b8b
Karabo Edward Mokgatle <karaboedward64@gmail.com>
2024-06-05 15:52:23 +0200

    added a const param and console logged params.

--- src/components/VanDetail.jsx
+++ src/components/VanDetail.jsx
@@ -2,5 +2,7 @@ import React from "react"
 import { useParams } from "react-router-dom"
 
 export default function vanDetail() {
+    const params = useParams()
+    console.log(params)
     return <h1>Van detail page goes here</h1>
 }
