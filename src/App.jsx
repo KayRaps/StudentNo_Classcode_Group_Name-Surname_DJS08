7f00c2746533782bb27884d223c90c3a66b86dba
Karabo Edward Mokgatle <karaboedward64@gmail.com>
2024-06-06 13:48:28 +0200

    added imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,39 @@
-import React from "react"; 
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
-import Home from "./components/Home"
-import About from "./components/About"
-import Vans from "./components/Vans"
-import VanDetail from "./components/VanDetail"
-import "./index.css";
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Vans from "./pages/Vans/Vans"
+import VanDetail from "./pages/Vans/VanDetail"
+import Dashboard from "./pages/Host/Dashboard"
+import Income from "./pages/Host/Income"
+import Reviews from "./pages/Host/Reviews"
+import Layout from "./components/Layout"
+
+import './index.css';
 
 import "./server"
 
 function App() {
-    return (
-      <BrowserRouter>
-      <header>
-        <Link className = "site-logo" to = "/">#VanLife</Link>
-        <nav>
-          <Link to = "/about">About</Link>
-          <Link to = "/vans">Vans</Link>
-        </nav>
-      </header>
-        <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/about" element={<About />}/>
-          <Route path="/vans" element={<Vans />}/>
-          <Route path="/vans/:id" element={<VanDetail/>}/>
-
-        </Routes>
-      </BrowserRouter>
-    )
-  }
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
+          <Route path="/host" element={<Dashboard />} />
+          <Route path="/host/income" element={<Income />} />
+          <Route path="/host/reviews" element={<Reviews />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
 
 ReactDOM
   .createRoot(document.getElementById('root'))
   .render(<App />);
 
-export default App
+  export default (App)
