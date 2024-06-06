38546e023ca40951a7791c27a72c365d286431c7
Karabo Edward Mokgatle <karaboedward64@gmail.com>
2024-06-06 15:20:04 +0200

    added import from Layout

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Dashboard from "./pages/Host/Dashboard"
 import Income from "./pages/Host/Income"
 import Reviews from "./pages/Host/Reviews"
 import Layout from "./components/Layout"
+import HostLayout from "./components/Layout"
 
 import "./server"
 
