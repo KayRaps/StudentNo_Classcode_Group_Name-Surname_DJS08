c7d7f7728acb26517b339d50eaa09f308cadd5be
Karabo Edward Mokgatle <karaboedward64@gmail.com>
2024-06-08 17:10:50 +0200

    added Outlet and imported it

--- src/pages/Host/Dashboard.jsx
+++ src/pages/Host/Dashboard.jsx
@@ -1,11 +1,7 @@
-import React from "react"
-import {Outlet} from "react-router-dom"
+//import React from "react"
 
 export default function Dashboard() {
     return (
-        <>
         <h1>Dashboard goes here</h1>
-        <Outlet />
-        </>
     )
 }
