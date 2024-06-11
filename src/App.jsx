40da426a8f07ce53d07e854d6d663238f9543212
Karabo Edward Mokgatle <karaboedward64@gmail.com>
2024-06-11 09:37:50 +0200

    added a function

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
-import Vans from "./pages/Vans/Vans"
+import Vans from "../Vans"
 import VanDetail from "./pages/Vans/VanDetail"
 import Dashboard from "./pages/Host/Dashboard"
 import Income from "./pages/Host/Income"
